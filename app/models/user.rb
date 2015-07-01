f83d74cf66bf326d28caa88ace001fe0ceb36f80
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-06-30 21:18:57 -0700

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  #validate that a name is entered
+  validates :name, :email, presence: true
+  #validates if the email has a correct format
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   has_secure_password
   belongs_to :location
   has_many :stocks
