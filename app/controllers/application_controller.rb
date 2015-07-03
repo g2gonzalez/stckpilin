8edb42f312e29cb5ac70d1cc0c1101a3a388b3c0
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-07-02 21:04:17 -0700

    Added crud links to stocks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  # allows us to use @current_user in our view files
   helper_method :current_user
 
   def authorize
-    @user_id = params[:id].to_i
-    if @user_id != current_user
+    if not current_user
       redirect_to login_path, flash: { danger: "Whoa buddy. That page is off limits!" }
     end
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ActiveRecord::Base
-	mount_uploader :stck_img, StckImgUploader
-	belongs_to :user
-	belongs_to :category
-	belongs_to :store
+  mount_uploader :stck_img, StckImgUploader
+  belongs_to :user
+  belongs_to :category
+  belongs_to :store
 end
